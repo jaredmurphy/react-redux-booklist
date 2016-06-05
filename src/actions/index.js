ed4df8ef783d47fb027e2502f0ada5d56119dee5
Jared <jaredcmurphy@gmail.com>
2016-06-04 23:17:06 -0400

    user clicks on book, talks to action, action talks to reduce and changes state of active book, react rereads state and active book renders on page

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-  console.log("A book has been selected", book.title);
+  // selectBook is an actioncreator and needs to reuturn
+  // an object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
